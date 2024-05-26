2c066c5035adc2b94432963c80b40eced1b0b57b
diyaralyasiri <diyaralyasiri@outlook.com>
2024-05-26 23:15:05 +0400

    initial commit

---
+++ ice_breaker.py
@@ -0,0 +1,53 @@
+from typing import Tuple
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+from third_party.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookupagent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser, Summary
+
+def ice_break_with(name: str)-> Tuple[Summary, str]:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data= scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=True)
+    # linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/diyar-alyasiri/", mock=True)
+
+    summary_template= """
+    Given the Linkedin infomration {information} about a person I want you to create:
+    1. A Short Summary
+    2. Two interesting facts about them
+    \n{format_instructions}
+    """
+   
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template,
+                                        partial_variables={"format_instructions": summary_parser.get_format_instructions()})
+
+    llm= ChatOpenAI(temperature=0, model_name="gpt-4o")
+
+    # chain= LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    sequence = summary_prompt_template | llm | summary_parser
+
+
+    # res=chain.invoke(input={"information":information})
+
+    # print(res)
+    
+    # try:
+    #     result:Summary = sequence.invoke({"information": linkedin_data})
+    #     print(result)
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
+
+    result:Summary = sequence.invoke({"information": linkedin_data})
+    
+    return result, linkedin_data.get("profile_pic_url")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print('Ice Breaker Enter')
+    ice_break_with(name="Diyar Alyasiri Linkedin")
+
+
+   
---
+++ third_party/linkedin.py
@@ -0,0 +1,62 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """Scrape info from linkedin profiles,
+    manually scrape the info from linkedin profiles.§
+    """
+    try:
+        if mock:
+            print("mock")
+            linkedin_profile_url = "https://gist.githubusercontent.com/diyaralyasiri/cc29fdafa18e936b16e6c907d014610d/raw/c946e728758b779d4f33b79a3e3430a90474abe5/diyar.json"
+            response = requests.get(
+                linkedin_profile_url,
+                timeout=10,
+            )
+        else:
+            api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+            header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+            response = requests.get(
+                api_endpoint,
+                params={"url": linkedin_profile_url},
+                headers=header_dic,
+                timeout=10,
+            )
+        response.raise_for_status()
+        
+        try:
+            data = response.json()
+        except requests.exceptions.JSONDecodeError:
+            print("Error: Response is not in JSON format")
+            print(f"Response text: {response.text}")
+            data = None  
+    except requests.exceptions.RequestException as e:
+        print(f"HTTP request failed: {e}")
+        data = None
+    
+        
+    data=response.json()
+    
+    data = {
+    k: v
+    for k, v in data.items()
+    if v not in ([], "", "", None)
+    and k not in ["people_also_viewed", "certifications"]
+    }
+    
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+    
+    return data
+        
+if __name__ =="__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/diyar-alyasiri/", mock = True
+        )
+    )
