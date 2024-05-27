6bacd862a81896f3cf8196068c01fcaaa8e78915
diyaralyasiri <diyaralyasiri@outlook.com>
2024-05-27 20:14:26 +0400

    fixed prompts

--- agents/linkedin_lookupagent.py
+++ agents/linkedin_lookupagent.py
@@ -25,10 +25,8 @@ def lookup(name:str)-> str:
     llm= ChatOpenAI(temperature=0, model_name="gpt-4o")
     
     template = """given the full name {name_of_person} I want you to get me a link to their Linkedin profile page.
-                            the domain should be 'linkedin.com/in'
-                            be careful not to provide me a link to a post, but rather to the profile itself.
-                            Do not give me a directory link either.
-                            Your answer should contain only a URL for linkedin"""
+                            the site/domain should be specified as 'site:linkedin.com/in'
+                            Your answer should contain only the URL"""
 
     prompt_template = PromptTemplate(
         template=template, input_variables=["name_of_person"]
